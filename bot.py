032983aec5f6be1b5348db16c1e26aa279e98a71
ak4r1s <57796478+amkaa1@users.noreply.github.com>
2026-03-12 19:41:40 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,34 @@
+from aiogram import Bot, Dispatcher, executor
+import config
+import database
+
+from handlers import start
+from handlers import profile
+from handlers import rating
+from handlers import invite
+from handlers import reward
+from handlers import scam
+from handlers import admin
+
+bot = Bot(token=config.BOT_TOKEN)
+dp = Dispatcher(bot)
+
+
+start.register(dp)
+profile.register(dp)
+rating.register(dp)
+invite.register(dp)
+reward.register(dp)
+scam.register(dp)
+admin.register(dp)
+
+
+async def on_startup(dp):
+
+    await database.connect()
+    await database.create_tables()
+
+
+if __name__ == "__main__":
+
+    executor.start_polling(dp, on_startup=on_startup)
