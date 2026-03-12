b58fc3cdf7ccf0b704b4648b1f50ebda787cb716
ak4r1s <57796478+amkaa1@users.noreply.github.com>
2026-03-12 19:36:30 +0800

    Create database.py

---
+++ database.py
@@ -0,0 +1,70 @@
+import asyncpg
+from config import DATABASE_URL
+
+pool = None
+
+async def connect():
+    global pool
+    pool = await asyncpg.create_pool(postgresql://postgres:rIabJsYSsNSETCAGpuqkPyhtZXPrjcOI@metro.proxy.rlwy.net:15476/railway)
+
+
+async def create_tables():
+
+    async with pool.acquire() as conn:
+
+        await conn.execute("""
+
+        CREATE TABLE IF NOT EXISTS users(
+            telegram_id BIGINT PRIMARY KEY,
+            username TEXT,
+            invites INT DEFAULT 0,
+            good INT DEFAULT 0,
+            bad INT DEFAULT 0,
+            verified BOOLEAN DEFAULT FALSE,
+            scam_reports INT DEFAULT 0,
+            muted BOOLEAN DEFAULT FALSE,
+            joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+
+        """)
+
+        await conn.execute("""
+
+        CREATE TABLE IF NOT EXISTS invites(
+            inviter BIGINT,
+            invited BIGINT UNIQUE,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+
+        """)
+
+        await conn.execute("""
+
+        CREATE TABLE IF NOT EXISTS ratings(
+            voter BIGINT,
+            target BIGINT,
+            rating TEXT,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+
+        """)
+
+        await conn.execute("""
+
+        CREATE TABLE IF NOT EXISTS scam_reports(
+            reporter BIGINT,
+            target BIGINT,
+            reason TEXT
+        )
+
+        """)
+
+        await conn.execute("""
+
+        CREATE TABLE IF NOT EXISTS rewards(
+            user_id BIGINT,
+            reward_level INT,
+            claimed BOOLEAN DEFAULT FALSE
+        )
+
+        """)
