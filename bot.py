f9a2c1fce76caa989a1c77a5a61995d8e7bac29c
ak4r1s <57796478+amkaa1@users.noreply.github.com>
2026-03-12 23:51:39 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -326,7 +326,5 @@ async def welcome(message:types.Message):
 async def on_startup(dp):
     await connect_db()
 
-if __name__=="__main__":
-    executor.start_polling(dp,on_startup=on_startup)
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
