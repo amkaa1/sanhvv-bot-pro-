be19c207fcad228cfc3c04bf5307ce554d313a7c
ak4r1s <57796478+amkaa1@users.noreply.github.com>
2026-03-13 02:26:54 +0800

    Update invite.py

--- handlers/invite.py
+++ handlers/invite.py
@@ -2,18 +2,19 @@ from aiogram import types
 
 def register(dp):
 
-    @dp.message_handler(commands=['invite'])
+    @dp.message_handler(commands=["invite"])
     async def invite(message: types.Message):
 
         user_id = message.from_user.id
 
-        link = f"https://t.me/YOUR_BOT?start={user_id}"
+        link = f"https://t.me/sanhvvmgl2026_bot?start={user_id}"
 
-        text = f"""
-Invite friends and earn rewards
+        await message.reply(
+f"""
+Invite friends
 
 Your link:
+
 {link}
 """
-
-        await message.reply(text)
+)
