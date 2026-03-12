bb40483d5238c6d5060887ce0e2a06f3aa264d8a
ak4r1s <57796478+amkaa1@users.noreply.github.com>
2026-03-12 19:35:24 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+BOT_TOKEN = os.getenv("8642047096:AAEMRFv2b1SIRcudUBtUS5iWOQxMgvhYmE8")
+DATABASE_URL = os.getenv("postgresql://postgres:rIabJsYSsNSETCAGpuqkPyhtZXPrjcOI@metro.proxy.rlwy.net:15476/railway")
+
+BOT_USERNAME = "sanhvvmgl2026_bot"
+
+ADMIN_USERNAME = "n3v3rmor31"
