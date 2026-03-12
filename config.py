9fbf1a676213972832044d5ddd3b55e59ac1c50e
ak4r1s <57796478+amkaa1@users.noreply.github.com>
2026-03-12 20:01:16 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-BOT_TOKEN = os.getenv("8642047096:AAEMRFv2b1SIRcudUBtUS5iWOQxMgvhYmE8")
-DATABASE_URL = os.getenv("postgresql://postgres:rIabJsYSsNSETCAGpuqkPyhtZXPrjcOI@metro.proxy.rlwy.net:15476/railway")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 BOT_USERNAME = "sanhvvmgl2026_bot"
 
