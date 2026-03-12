d2436d1917901fdc6e92655f26f56c621336c1ab
ak4r1s <57796478+amkaa1@users.noreply.github.com>
2026-03-12 20:08:41 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,8 +27,5 @@ async def on_startup(dp):
 
     await database.connect()
     await database.create_tables()
-
-
 if __name__ == "__main__":
-
     executor.start_polling(dp, on_startup=on_startup)
