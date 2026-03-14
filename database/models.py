146de4a8d19c0c7465d5e7c3114d4e20badd4300
ak4r1s <57796478+amkaa1@users.noreply.github.com>
2026-03-15 00:56:33 +0800

    Create models.py

---
+++ database/models.py
@@ -0,0 +1,46 @@
+from .db import pool
+
+async def create_tables():
+
+    async with pool.acquire() as conn:
+
+        await conn.execute("""
+
+        CREATE TABLE IF NOT EXISTS users(
+
+        user_id BIGINT PRIMARY KEY,
+        username TEXT,
+        invited_by BIGINT,
+        invites INT DEFAULT 0,
+        good INT DEFAULT 0,
+        bad INT DEFAULT 0,
+        verified BOOLEAN DEFAULT FALSE
+
+        )
+
+        """)
+
+        await conn.execute("""
+
+        CREATE TABLE IF NOT EXISTS ratings(
+
+        voter BIGINT,
+        target BIGINT,
+        type TEXT,
+        time TIMESTAMP DEFAULT NOW()
+
+        )
+
+        """)
+
+        await conn.execute("""
+
+        CREATE TABLE IF NOT EXISTS invites(
+
+        inviter BIGINT,
+        invited BIGINT,
+        valid BOOLEAN DEFAULT TRUE
+
+        )
+
+        """)
